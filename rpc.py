36c8b1b2946db395d4435e554115b1c24804f5b6
blue-94 <jtenn84@gmail.com>
2025-05-01 11:55:25 -0700

    Change "Playing cmus" to "Listening to cmus"

--- rpc.py
+++ rpc.py
@@ -1,4 +1,5 @@
-from pypresence import Presence
+from pypresence import Presence, ActivityType
+import io
 from math import floor
 import time
 import os
@@ -15,6 +16,22 @@ link = ""
 # start rich presence
 RPC = Presence(client_id, pipe=0)
 RPC.connect()
+
+# extracts cover from mp3 and saves it
+def getImage(file, RPC):
+    # https://unix.stackexchange.com/questions/41287/how-to-extract-album-cover-image-from-mp3-file
+    os.system('ffmpeg -i "' + file + '" -an -c:v copy file.jpg -y -hide_banner -loglevel error')
+    RPC.update(large_image =  str(os.system('curl  -s -F "reqtype=fileupload" -F "time=1h" -F "fileToUpload=@file.jpg" https://litterbox.catbox.moe/resources/internals/api.php'))[:-1])
+    RPC.update(large_image = link)
+
+# coverts the given seconds to minutes
+def convertToMin(sec):
+    minutes = floor(sec / 60)
+    seconds = sec - minutes * 60
+    if seconds < 10:
+        return str(minutes), ("0" + str(seconds))
+    else:
+        return str(minutes), str(seconds)
 
 # while program runs update status every TIME seconds
 while True:
@@ -53,12 +70,15 @@ while True:
         link = litterbox[0]
         l.close()
         change = True        
-        
+
+    #curr_min, curr_sec = convertToMin(int(lines[3][8:]))
+    #end_min, end_sec = convertToMin(int(lines[2][8:]))
     f.close()
 
     # update status if new song as of rn
     if change == True:
         RPC.update(
+                activity_type = ActivityType.LISTENING,
                 details = song,
                 state = "by" + artist,
                 #state = curr_min + ":" + curr_sec + "/" + end_min + ":" + end_sec, 
